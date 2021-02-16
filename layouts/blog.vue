7053c22667ddf59b009891f6977b6d5bed2646f8
Fapalz <blacesmot@gmail.com>
2021-02-16 16:30:16 +0500

    chore: init commit

---
+++ layouts/blog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Блог</h1>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="articles">Тут будут статьи...</div>
+</template>
+
+<script>
+export default {
+  layout: 'blog',
+}
+</script>
+
+<style>
+.articles {
+  color: blue;
+}
+</style>
